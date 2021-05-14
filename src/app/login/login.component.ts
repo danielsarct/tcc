c722ff994b600461f93521bd4ef8c532e26c2679
Daniel Soares <danielsarct@gmail.com>
2021-05-14 14:37:43 -0300

    tentiva de navegacao telas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,5 +13,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+
 
 }
